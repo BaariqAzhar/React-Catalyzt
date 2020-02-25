556ab45b55148d6c4cc940838c54be5d71d6c856
BaariqAzhar <36961071+BaariqAzhar@users.noreply.github.com>
2020-02-25 12:08:07 +0700

    add discover slide

--- src/views/CatalyztPage/DiscoverSection.js
+++ src/views/CatalyztPage/DiscoverSection.js
@@ -22,11 +22,10 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import image1 from "assets/img/examples/olu-eletu.jpg";
 import { Router, Route, Switch, Link } from "react-router-dom";
 // import { Link } from "@material-ui/core";
 
-import slide1 from 'assets/img/catalyzt/discover-slide/I Want To Be/1.png'
+// import slide1 from 'assets/img/catalyzt/discover-slide/I Want To Be/1.png'
 
 //modal
 // import modalStyle from "assets/jss/material-dashboard-pro-react/modalStyle.js";
@@ -36,6 +35,15 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
+import Carousel from "react-slick";
+import LocationOn from "@material-ui/icons/LocationOn";
+import slide1 from "assets/img/catalyzt/discover-slide/I Want To Be/1.png";
+import slide2 from "assets/img/catalyzt/discover-slide/I Want To Be/2.jpg";
+import slide3 from "assets/img/catalyzt/discover-slide/I Want To Be/3.jpg";
+import slide4 from "assets/img/catalyzt/discover-slide/I Want To Be/4.jpg";
+import slide5 from "assets/img/catalyzt/discover-slide/I Want To Be/5.jpg";
+import slide6 from "assets/img/catalyzt/discover-slide/I Want To Be/6.jpg";
+
 // @material-ui/icons
 import Close from "@material-ui/icons/Close";
 
@@ -48,54 +56,18 @@ const useStyles = makeStyles(styles);
 
 export default function DiscoverSection() {
     const [modal, setModal] = React.useState(false);
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    };
 
     const classes = useStyles();
     return (
         <div className={classes.section}>
-            <Dialog
-                classes={{
-                    root: classes.center,
-                    paper: classes.modal
-                }}
-                open={modal}
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={() => setModal(false)}
-                aria-labelledby="modal-slide-title"
-                aria-describedby="modal-slide-description"
-            >
-                <DialogTitle
-                    id="classic-modal-slide-title"
-                    disableTypography
-                    className={classes.modalHeader}
-                >
-                    <IconButton
-                        className={classes.modalCloseButton}
-                        key="close"
-                        aria-label="Close"
-                        color="inherit"
-                        onClick={() => setModal(false)}
-                    >
-                        <Close className={classes.modalClose} />
-                    </IconButton>
-                    <h4 className={classes.modalTitle}>Modal title</h4>
-                </DialogTitle>
-                <DialogContent
-                    id="modal-slide-description"
-                    className={classes.modalBody}
-                >
-                    <h5>Are you sure you want to do this?</h5>
-                </DialogContent>
-                <DialogActions
-                    className={classes.modalFooter + " " + classes.modalFooterCenter}
-                >
-                    <Button onClick={() => setModal(false)}>Never Mind</Button>
-                    <Button onClick={() => setModal(false)} color="success">
-                        Yes
-                    </Button>
-                </DialogActions>
-            </Dialog>
-
             <GridContainer justify="center">
                 <GridItem xs={12} sm={12} md={8}>
                     <h2 className={classes.title}>Discover</h2>
@@ -161,6 +133,101 @@ export default function DiscoverSection() {
                     <GridItem xs={12} sm={12} md={4}>
                     </GridItem>
                 </GridContainer>
+                {/* Modal of Discover slide */}
+                <Dialog
+                    classes={{
+                        root: classes.center,
+                        paper: classes.modal
+                    }}
+                    open={modal}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={() => setModal(false)}
+                    aria-labelledby="modal-slide-title"
+                    aria-describedby="modal-slide-description"
+                >
+                    <DialogTitle
+                        id="classic-modal-slide-title"
+                        disableTypography
+                        className={classes.modalHeader}
+                    >
+                        <IconButton
+                            className={classes.modalCloseButton}
+                            key="close"
+                            aria-label="Close"
+                            color="inherit"
+                            onClick={() => setModal(false)}
+                        >
+                            <Close className={classes.modalClose} />
+                        </IconButton>
+                        {/* <h4 className={classes.modalTitle}>Modal title</h4> */}
+                    </DialogTitle>
+                    <DialogContent
+                        id="modal-slide-description"
+                        className={classes.modalBody}
+                    >
+                        {/* <h5>Are you sure you want to do this?</h5> */}
+                        {/* <GridContainer>
+                            <GridItem xs={12} sm={12} md={8}>
+                                <Card> */}
+                        <Carousel {...settings}>
+                            <div>
+                                <img
+                                    src={slide1}
+                                    alt="First slide"
+                                    className="slick-image"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    src={slide2}
+                                    alt="Second slide"
+                                    className="slick-image"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    src={slide3}
+                                    alt="Third slide"
+                                    className="slick-image"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    src={slide4}
+                                    alt="Forth slide"
+                                    className="slick-image"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    src={slide5}
+                                    alt="Fifth slide"
+                                    className="slick-image"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    src={slide6}
+                                    alt="Sixth slide"
+                                    className="slick-image"
+                                />
+                            </div>
+                        </Carousel>
+                        {/* </Card>
+                            </GridItem>
+                        </GridContainer> */}
+                    </DialogContent>
+                    {/* <DialogActions
+                        className={classes.modalFooter + " " + classes.modalFooterCenter}
+                    >
+                        <Button onClick={() => setModal(false)}>Never Mind</Button>
+                        <Button onClick={() => setModal(false)} color="success">
+                            Yes
+                    </Button>
+                    </DialogActions> */}
+                </Dialog>
+                {/*  */}
             </div>
         </div>
     );
